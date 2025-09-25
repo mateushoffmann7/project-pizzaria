c2459d39f4e478fc724fa74f9ef6b78180ec4a4f
Mettel07 <mateushoffmann0707@gmail.com>
2025-09-24 22:27:21 -0300

    add modal infos

--- pizzas.js
+++ pizzas.js
@@ -1,64 +1,64 @@
 let pizzaJson = [
   {
-    id: 1,
+    id: 0,
     name: "Mussarela",
     img: "./assets/pizza.png",
     price: 20.19,
-    sizes: ["8 Pedaços", "12 Pedaços", "16 Pedaços"],
+    sizes: ["8 Pç", "12 Pç", "16 Pç"],
+    description:
+      "Descrição da pizza em mais de uma linha muito legal bem interessante",
+  },
+  {
+    id: 1,
+    name: "Calabresa",
+    img: "./assets/pizza2.png",
+    price: 18.0,
+    sizes: ["8 Pç", "12 Pç", "16 Pç"],
     description:
       "Descrição da pizza em mais de uma linha muito legal bem interessante",
   },
   {
     id: 2,
-    name: "Calabresa",
-    img: "./assets/pizza2.png",
-    price: 18.0,
-    sizes: ["8 Pedaços", "12 Pedaços", "16 Pedaços"],
+    name: "Quatro Queijos",
+    img: "./assets/pizza3.png",
+    price: 17.45,
+    sizes: ["8 Pç", "12 Pç", "16 Pç"],
     description:
       "Descrição da pizza em mais de uma linha muito legal bem interessante",
   },
   {
     id: 3,
-    name: "Quatro Queijos",
-    img: "./assets/pizza3.png",
-    price: 17.45,
-    sizes: ["8 Pedaços", "12 Pedaços", "16 Pedaços"],
+    name: "Americana",
+    img: "./assets/pizza4.png",
+    price: 19.77,
+    sizes: ["8 Pç", "12 Pç", "16 Pç"],
     description:
       "Descrição da pizza em mais de uma linha muito legal bem interessante",
   },
   {
     id: 4,
-    name: "Americana",
-    img: "./assets/pizza4.png",
-    price: 19.77,
-    sizes: ["8 Pedaços", "12 Pedaços", "16 Pedaços"],
+    name: "Sorvete",
+    img: "./assets/pizza5.png",
+    price: 21.43,
+    sizes: ["8 Pç", "12 Pç", "16 Pç"],
     description:
       "Descrição da pizza em mais de uma linha muito legal bem interessante",
   },
   {
     id: 5,
-    name: "Sorvete",
-    img: "./assets/pizza5.png",
-    price: 21.43,
-    sizes: ["8 Pedaços", "12 Pedaços", "16 Pedaços"],
+    name: "Moda da Casa",
+    img: "./assets/pizza6.png",
+    price: 18.55,
+    sizes: ["8 Pç", "12 Pç", "16 Pç"],
     description:
       "Descrição da pizza em mais de uma linha muito legal bem interessante",
   },
   {
     id: 6,
-    name: "Moda da Casa",
-    img: "./assets/pizza6.png",
-    price: 18.55,
-    sizes: ["8 Pedaços", "12 Pedaços", "16 Pedaços"],
-    description:
-      "Descrição da pizza em mais de uma linha muito legal bem interessante",
-  },
-  {
-    id: 7,
     name: "Chocolate",
     img: "./assets/pizza7.png",
     price: 22.36,
-    sizes: ["8 Pedaços", "12 Pedaços", "16 Pedaços"],
+    sizes: ["8 Pç", "12 Pç", "16 Pç"],
     description:
       "Descrição da pizza em mais de uma linha muito legal bem interessante",
   },
--- script.js
+++ script.js
@@ -2,12 +2,39 @@
 pizzaJson.map((i, index) => {
   //clona o modelo do html - multiplicando pela quantidade de itens no loop
   let pizzaItem = document.querySelector(".models .pizza-item").cloneNode(true);
+
+  pizzaItem.setAttribute("data-key", i.id);
   pizzaItem.querySelector(".pizza-item--name").innerHTML = i.name;
   pizzaItem.querySelector(".pizza-item--desc").innerHTML = i.description;
   pizzaItem.querySelector(
     ".pizza-item--price"
   ).innerHTML = `R$ ${i.price.toFixed(2)}`;
   pizzaItem.querySelector(".pizza-item--img img").setAttribute("src", i.img);
+  pizzaItem.querySelector("a").addEventListener("click", (e) => {
+    e.preventDefault();
+    let key = e.target.closest(".pizza-item").getAttribute("data-key");
+
+    document.querySelector(".pizzaInfo h1").innerHTML = pizzaJson[key].name;
+    document.querySelector(".pizzaInfo--desc").innerHTML =
+      pizzaJson[key].description;
+    document
+      .querySelector(".pizzaBig img")
+      .setAttribute("src", pizzaJson[key].img);
+    document.querySelector(
+      ".pizzaInfo--actualPrice"
+    ).innerHTML = `R$ ${pizzaJson[key].price.toFixed(2)}`;
+
+    document.querySelectorAll(".pizzaInfo--size").forEach((size, sizeIndex) => {
+      size.querySelector("span").innerHTML = pizzaJson[key].sizes[sizeIndex];
+    });
+
+    document.querySelector(".pizzaWindowArea").style.display = "flex";
+    document.querySelector(".pizzaWindowArea").style.opacity = "0";
+    setTimeout(() => {
+      document.querySelector(".pizzaWindowArea").style.opacity = "1";
+    }, 200);
+  });
+
   //coloca na tela
   document.querySelector(".pizza-area").append(pizzaItem);
 });
